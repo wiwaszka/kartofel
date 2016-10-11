89b96241c519ee2b40bd80c125131dba479518db
wiwaszka <wiwaszkiewicz66@gmail.com>
2016-10-11 20:01:48 +0200

    plansza
    grafiki i kod rysujący

--- kartofel.rb
+++ kartofel.rb
@@ -14,6 +14,17 @@ class GameWindow < Gosu::Window
   end
 
   def draw
+    @plansza.each_with_index do |wiersz, numer_wiersza|
+      wiersz.each_with_index do |pole, numer_kolumny|
+        case pole
+        when :tlo
+          @poziom [0].draw 32*numer_kolumny, 32*numer_wiersza, 0
+        when :grunt
+          @poziom[1].draw 32*numer_kolumny, 32*numer_wiersza, 0
+        when :brzeg
+          @poziom[2].draw 32*numer_kolumny, 32*numer_wiersza, 0
+        when :drzwi
+          @drzwi[0].draw 32*numer-kolumny, 32*numer_wiersza
   end
 end
 
